2bea1af8c55502697aab3b7166239f1dff244e58
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-19 15:43:42 -0300

    adicioando sprite manualmente

--- 05 - FlappyBirds/sprite.js
+++ 05 - FlappyBirds/sprite.js
@@ -26,5 +26,47 @@ Sprite.prototype.draw = function(ctx, x ,y){
 };
 
 function initSprites(img){
+    s_bird = [
+        new Sprite(img, 156, 115, 17, 12),
+        new Sprite(img, 156, 128, 17, 12),
+        new Sprite(img, 156, 141, 17, 12)
+    ];
 
+    s_bg = new Sprite(img, 0, 0, 139, 114);
+    s_bg.color = "#70C5CF";
+    s_fg = new Sprite(img, 138, 0, 112, 56);
+
+    s_pipeNorth = new Sprite(img, 251, 0, 26, 200);
+    s_pipeSouth = new Sprite(img, 277, 0, 26, 200);
+
+    s_text = {
+        FlappyBird: new Sprite(img, 59, 114, 96, 22),
+        GameOver:   new Sprite(img, 59, 136, 94, 19),
+        GetReady:   new Sprite(img, 59, 155, 87, 22)
+    }
+    s_buttons = {
+        Rate:   new Sprite(img,  79, 177, 40, 14),
+        Menu:   new Sprite(img, 119, 177, 40, 14),
+        Share:  new Sprite(img, 159, 177, 40, 14),
+        Score:  new Sprite(img,  79, 191, 40, 14),
+        Ok:     new Sprite(img, 119, 191, 40, 14),
+        Start:  new Sprite(img, 159, 191, 40, 14)
+    }
+
+    s_score = new Sprite(img, 138,  59, 113, 58);
+    s_plash = new Sprite(img,   0, 114,  59, 49);
+
+    s_numberS = new Sprite(img, 0, 177, 6,  7);
+    s_numberB = new Sprite(img, 0, 188, 7, 10);
+
+    s_numberS.draw = s_numberB.draw = function(ctx, x, y, num){
+        num = num.toString();
+        var step = this.width + 2;
+        for (var i = 0, len = num.length; i < len; i++){
+            var n = parseInt(num[i]);
+            ctx.drawImage(img, step*n, this.y, this.width, this.height,
+                x, y, this.width, this.height)
+                x += step;
+        }
+    }
 }
