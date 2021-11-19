51e34c618e7ca58ea9d9f2ad27b654340a5982de
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-19 08:13:25 -0300

    setup sprite and drawSprite

--- 04 - SPACE INVADERS/helpers.js
+++ 04 - SPACE INVADERS/helpers.js
@@ -7,16 +7,20 @@ function Screen(width, height){
     this.canvas.width = this.width = width;
     this.canvas.height = this.height = height;
     this.ctx = this.canvas.getContext("2d");
-    
+
 }
 
 Screen.prototype.drawSprite = function(sp, x, y){
-
+    this.ctx.drawImage(sp.img, sp.x, sp.y,sp.w,sp.h, x, y, sp.w, sp.h);
 }
 
 //SPRITE
 function Sprite(img, x, y, w,h){
-
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
 }
 
 //INPUTHANDELER
