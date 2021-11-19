ab71e46118f546ac1679a42f6c2177947b6f4e60
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-19 08:10:43 -0300

    function screen

--- 04 - SPACE INVADERS/helpers.js
+++ 04 - SPACE INVADERS/helpers.js
@@ -3,7 +3,11 @@
 
 //SCREEN
 function Screen(width, height){
-
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = this.width = width;
+    this.canvas.height = this.height = height;
+    this.ctx = this.canvas.getContext("2d");
+    
 }
 
 Screen.prototype.drawSprite = function(sp, x, y){
