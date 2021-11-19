89196a9c26a9a90c211dd40c8090c5dacfb9da77
Renan Rodrigues <renan_ime_2012@hotmail.com>
2021-11-19 01:43:09 -0300

    create intro helpers

---
+++ 04 - SPACE INVADERS/helpers.js
@@ -0,0 +1,25 @@
+//HELPER FUNCTIONS
+
+
+//SCREEN
+function Screen(width, height){
+
+}
+
+Screen.prototype.drawSprite = function(sp, x, y){
+
+}
+
+//SPRITE
+function Sprite(img, x, y, w,h){
+
+}
+
+//INPUTHANDELER
+function InputHandeler(){};
+
+InputHandeler.prototype.isDown = function(code){
+
+}
+
+//3:30
