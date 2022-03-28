08d3a599bba44feb657461c1d8e5e85371003480
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-03-28 14:12:32 -0300

    draw character spritesheet

--- 08 - JRPG Pizza Legends/Overworld.js
+++ 08 - JRPG Pizza Legends/Overworld.js
@@ -6,11 +6,33 @@ class Overworld{
     }
 
     init(){
+
+        //map
         const image = new Image();
         image.onload = () =>{
              this.ctx.drawImage(image,0,0);
 
         };
         image.src = "./images/maps/DemoLower.png";
+
+
+        //hero
+        const x = 0;
+        const y = 0;
+        const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero,
+                0,//left cut
+                0,//top cut
+                32,//width of cut
+                32,//height of cut
+                x,
+                y,
+                32,
+                32
+                );
+        };
+        hero.src = "./images/characters/people/hero.png"
     }
 }
