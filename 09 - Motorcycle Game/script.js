f05dff2f8ee17ca0b31cd88692c08db8dd1d1ad3
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-03-29 05:27:27 -0300

    noise function

--- 09 - Motorcycle Game/script.js
+++ 09 - Motorcycle Game/script.js
@@ -4,6 +4,15 @@ c.width = 500;
 c.height = 350;
 document.body.appendChild(c);
 
+var perm = [];
+while (perm.length < 255){
+    while(perm.includes(val = Math.floor(Math.random()*255)));
+    perm.push(val)
+}
+var noise =x=>{
+    return
+}
+
 function loop(){
     ctx.fillStyle = "#19f";
     ctx.fillRect(0,0,c.width,c.height);
