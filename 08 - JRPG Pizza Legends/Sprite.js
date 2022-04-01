9cd607eb6e7c2707524903fb0f5856d69ba94208
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-01 15:24:52 -0300

    sprite js

--- 08 - JRPG Pizza Legends/Sprite.js
+++ 08 - JRPG Pizza Legends/Sprite.js
@@ -1,6 +1,13 @@
 class Sprite{
     constructor(config){
 
+        //Set up the Image
+        this.image = new Image();
+        this.image.src = config.src;
+        this.image.onload = () => {
+            this.isLoaded = true;
+        }
+
         //config Animation & Initial state
         this.animations = config.animations || {
             idleDown:[
@@ -9,8 +16,21 @@ class Sprite{
         }
         this.currentAnimation = config.currentAnimation || "idleDown";
         this.currentAnimationFrame = 0;
+
+        //Reference the game object
+        this.gameObject = config.gameObject;
+    }
+
+    draw(ctx){
+        const x = this.gameObject.x * 16 - 8;
+        const y = this.gameObject.y * 16 - 18;
+
+        ctx.drawImage(this.image,
+            0, 0,
+            32, 32,
+            x, y,
+            32, 32
+            )
     }
 }
 
-
-7:03
