22e610701113bdcca817f10f4ee04502b9944c2a
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-05 12:15:28 -0300

    map examples

--- 08 - JRPG Pizza Legends/OverworldMap.js
+++ 08 - JRPG Pizza Legends/OverworldMap.js
@@ -6,14 +6,52 @@ class OverworldMap{
         this.lowerImage.src = config.lowerSrc;
 
         this.upperImage = new Image();
-        this.upperImage.src = config.upperImage;
-
-        drawLowerImage(ctx){
-            ctx.drawImage(this.lowerImage,0,0)
-        }
-
-        drawUpperImage(ctx){
-            ctx.drawImage(this.upperImage,0,0)
-        }
+        this.upperImage.src = config.upperSrc;
     }
+    drawLowerImage(ctx){
+        ctx.drawImage(this.lowerImage,0,0);
+    }
+
+    drawUpperImage(ctx){
+        ctx.drawImage(this.upperImage,0,0);
+    }
+}
+
+window.OverworldMap = {
+    DemoRoom:{
+        lowerSrc: "./images/maps/DemoLower.png",
+        upperSrc: "./images/maps/DemoUpper.png",
+        gameObject:{
+            hero: new GameObject({
+                x: 5,
+                y: 6,
+            }),
+            npc1: newGameObject({
+                x:7,
+                y:9,
+                src: "/images/characters/people/npc1.png"
+            })
+        }
+    },
+
+    Kitchen:{
+        lowerSrc: "./images/maps/KitchenLower.png",
+        upperSrc: "./images/maps/KitchenUpper.png",
+        gameObject:{
+            hero: new GameObject({
+                x: 3,
+                y: 1,
+            }),
+            npcA: newGameObject({
+                x:9,
+                y:2,
+                src: "/images/characters/people/npc1.png"
+            }),
+            npcB: newGameObject({
+                x:10,
+                y:4,
+                src: "/images/characters/people/npc2.png"
+            })
+        }
+    },
 }
