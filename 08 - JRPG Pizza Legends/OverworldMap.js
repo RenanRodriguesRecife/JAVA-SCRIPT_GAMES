62832e4140de5651d94a7a8f1f917d98fcc8e40f
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-22 16:26:57 -0300

    create functions to add remove and move wall colision to gameObjects

--- 08 - JRPG Pizza Legends/OverworldMap.js
+++ 08 - JRPG Pizza Legends/OverworldMap.js
@@ -24,6 +24,20 @@ class OverworldMap{
         const{x,y}=utils.nextPosition(currentX,currentY,direction);
         return this.walls[`${x},${y}`]||false;
     }
+
+    addWall(x,y){
+        this.walls[`${x},${y}`] = true;
+    }
+
+    removeWall(x,y){
+        delete this.walls[`${x},${y}`];
+    }
+
+    moveWall(wasX, wasY, direction){
+        this.removeWall(wasX, wasY);
+        const{x,y} = utils.nextPosition(wasX,wasY, direction);
+        this.addWall(x,y);
+    }
 }
 
 window.OverworldMaps = {
