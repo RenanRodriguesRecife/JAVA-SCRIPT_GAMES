449a78580710bb017ecc7201848ec94eb227697d
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-22 15:53:34 -0300

    create behavior refact to colision

--- 08 - JRPG Pizza Legends/Person.js
+++ 08 - JRPG Pizza Legends/Person.js
@@ -14,31 +14,53 @@ class Person extends GameObject{
     }
 
     update(state){
-        this.updatePosition();
-        this.updateSprite(state);
-        if(this.isPlayerControlled && this.movingProgressRemaining === 0 && state.arrow){
-            this.direction = state.arrow;
-            console.log(state.map.isSpaceTaken(this.x,this.y,this.direction));
+        if(this.movingProgressRemaining > 0){
+            this.updatePosition();
+        }else{
+
+
+
+
+            //Teclado liberado e arrow key pressed
+            if(this.isPlayerControlled && state.arrow){
+                this.startBehavior(state,{
+                    type:"walk",
+                    direction: state.arrow
+                })
+            }
+            this.updateSprite(state);
+        }
+    }
+
+    startBehavior(state, behavior){
+        //definir a direção do personagem para qualquer comportamento que ele tenha
+        this.direction = behavior.direction;
+        //Para aqui se o espaço não é livre
+        if(behavior.type === "walk"){
+            if(state.map.isSpaceTaken(this.x, this.y,this.direction)){
+                return;
+            }
+            //pronto para andar
             this.movingProgressRemaining = 16;
         }
     }
 
     updatePosition(){
-        if(this.movingProgressRemaining > 0){
+        
             const [property, change] = this.directionUpdate[this.direction];
             this[property] += change;
             this.movingProgressRemaining -=1;
-        }
+        
     }
 
     updateSprite(state){
 
-        if(this.isPlayerControlled && this.movingProgressRemaining === 0 && !state.arrow){
-            this.sprite.setAnimation("idle-"+this.direction);
-            return;
-        }
         if(this.movingProgressRemaining > 0){
             this.sprite.setAnimation("walk-"+this.direction);
+            return;
         }
+
+        this.sprite.setAnimation("idle-"+this.direction);
+        
     }
 }
