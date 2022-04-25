cc714c98909e40472d0bc2dcbf83c8ca734ff178
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-25 19:00:09 -0300

    behavior handle function

--- 08 - JRPG Pizza Legends/GameObject.js
+++ 08 - JRPG Pizza Legends/GameObject.js
@@ -9,15 +9,46 @@ class GameObject{
             gameObject:this,
             src: config.src || "./images/characters/people/hero.png",
         });
+
+        this.behaviorLoop = config.behaviorLoop || [];
+        this.behaviorLoopIndex = 0;
     }
 
     mount(map){
         console.log("mounting")
         this.isMounted = true;
         map.addWall(this.x, this.y);
+
+        //se tem um conportamento, incia depois de um pequeno atraso
+        setTimeout(()=>{
+            this.doBehaviorEvent(map);
+        },10)
     }
 
     update(){
 
+    }
+
+    async doBehaviorEvent(map){
+
+        if(map.isCutscenesPlaying || this.behaviorLoop.length === 0){
+            return;
+        }
+
+        //configurando a informação do evento com informação relevante
+        let event = this.behaviorLoop[this.behaviorLoopIndex];
+        eventConfig.who = this.id;
+
+        const eventHandler = new OverworldEvent({map, event: eventConfig});
+        await eventHandler.init();
+
+        //próximo evento para rodar
+        this.behaviorLoopIndex += 1;
+        if(this.behaviorLoopIndex === this.behaviorLoop.length){
+            this.behaviorLoopIndex = 0;
+        }
+
+        //faz tuto de novo
+        this.doBehaviorEvent(map);
     }
 }
