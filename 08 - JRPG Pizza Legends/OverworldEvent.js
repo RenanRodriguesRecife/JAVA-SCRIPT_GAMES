601260552b38e7480a47071fdeb668323abd1a6c
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-26 15:36:58 -0300

    walk behavior

--- 08 - JRPG Pizza Legends/OverworldEvent.js
+++ 08 - JRPG Pizza Legends/OverworldEvent.js
@@ -9,6 +9,18 @@ class OverworldEvent{
     }
 
     walk(resolver){
-        
+        const who = this.map.gameObjects[this.event.who];
+        who.startBehavior({
+            map: this.map
+        },{
+            type: "walk",
+            direction: this.event.direction
+        })
+    }
+
+    init(){
+        return new Promise(resolve => {
+            this[this.event.type](resolve)
+        })
     }
 }
