6d3b770cbf81ac3006d0ec978a1e4b7e5d8e98cf
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-05-22 18:24:42 -0300

    fix aspect ratio

--- 11 - Street Fighter JS/index.js
+++ 11 - Street Fighter JS/index.js
@@ -1,19 +1,16 @@
 const GameViewport = {
     WIDTH: 384,
     HEIGHT: 224,
-    SCALE: 3,
 }
 
 window.onload = function(){
     const canvasEl = document.querySelector('canvas');
     const context = canvasEl.getContext('2d');
 
-    canvasEl.width = 384;
-    canvasEl.height = 224;
+    canvasEl.width = GameViewport.WIDTH;
+    canvasEl.height = GameViewport.HEIGHT;
 
-    canvasEl.style.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
-    canvasEl.style.height = `${GameViewport.HEIGHT * GameViewport.SCALE}px`;
-
+    
     context.strokeStyle = 'yellow';
     context.moveTo(0, 0);
     context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
