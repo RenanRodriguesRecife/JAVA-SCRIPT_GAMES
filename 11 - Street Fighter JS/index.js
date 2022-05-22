cdbb00ca5ee9d2aa881f5d365124126b16231406
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-05-22 17:54:24 -0300

    draw lines

--- 11 - Street Fighter JS/index.js
+++ 11 - Street Fighter JS/index.js
@@ -1,7 +1,7 @@
 const GameViewport = {
     WIDTH: 384,
     HEIGHT: 224,
-    SCALE: 4,
+    SCALE: 3,
 }
 
 window.onload = function(){
@@ -13,6 +13,13 @@ window.onload = function(){
 
     canvasEl.style.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
     canvasEl.style.height = `${GameViewport.HEIGHT * GameViewport.SCALE}px`;
+
+    context.strokeStyle = 'yellow';
+    context.moveTo(0, 0);
+    context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+    context.moveTo(GameViewport.WIDTH, 0);
+    context.lineTo(0, GameViewport.HEIGHT);
+    context.stroke();
 
     console.log(context)
 }
