d0c9fe377e69ef5a72b7ff0be35601e37d97c91c
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-05-23 14:57:53 -0300

    frame loop

--- 11 - Street Fighter JS/index.js
+++ 11 - Street Fighter JS/index.js
@@ -10,13 +10,41 @@ window.onload = function(){
     canvasEl.width = GameViewport.WIDTH;
     canvasEl.height = GameViewport.HEIGHT;
 
+    const ryu = document.querySelector('img');
     
-    context.strokeStyle = 'yellow';
-    context.moveTo(0, 0);
-    context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
-    context.moveTo(GameViewport.WIDTH, 0);
-    context.lineTo(0, GameViewport.HEIGHT);
-    context.stroke();
+    function frame(){
+        context.strokeStyle = 'yellow';
+        context.moveTo(0, 0);
+        context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+        context.moveTo(GameViewport.WIDTH, 0);
+        context.lineTo(0, GameViewport.HEIGHT);
+        context.stroke();
+    
+        context.drawImage(ryu,0,0);
+        window.requestAnimationFrame(frame);
+    }
+
+    window.requestAnimationFrame(frame);
 
     console.log(context)
 }
+
+
+//função frame loop
+// exemplo ruim 1
+// while(running){
+//     doStuff();
+// }
+
+// exemplo ruim 1
+// setInterval(frame, 60/1000);
+// function frame(){
+//     doStuff();
+// }
+
+// bom exemplo
+// window.requestAnimationFrame(frame);
+// function frame(){
+//     doStuff();
+//     window.requestAnimationFrame(frame);
+// }
