f9aab6a1a2a177301f32bc1823eea39eb111b6ce
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-05-31 23:54:37 -0300

    minor fix

--- 11 - Street Fighter JS/src/ryu.js
+++ 11 - Street Fighter JS/src/ryu.js
@@ -1,20 +1,21 @@
-const ryu  = document.querySelectorAll('img[alt="ryu"]');
+const ryu  = document.querySelector('img[alt="ryu"]');
 
 const position = {
-    x: GameViewport.WIDTH/2 - ryu.width/2,
+    // x: GameViewport.WIDTH/2 - ryu.width/2,
+    x: 80,
     y: 110,
 }
 
 let velocity = 1;
 
-export function updateRyu(){
+export function updateRyu(context){
     position.x += velocity;
 
-    if(position.x > GameViewport.WIDTH - ryu.width|| position.x < 0){
+    if(position.x > context.canvas.width - ryu.width|| position.x < 0){
         velocity = -velocity;
     }
 }
 
-export function drawRyu(){
+export function drawRyu(context){
     context.drawImage(ryu,position.x,position.y);
 }
