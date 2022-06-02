460ddc3ca32721aaf161e38ea0a09f6da0caf319
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-01 23:57:57 -0300

    some organise

--- 11 - Street Fighter JS/src/ken.js
+++
@@ -1,8 +0,0 @@
-import { Fighter } from './fighter.js';
-
-export class Ken extends Fighter{
-    constructor(x, y, velocity){
-        super('Ken', x, y, velocity);
-        this.image = document.querySelector('img[alt="ken"');
-    }
-}
