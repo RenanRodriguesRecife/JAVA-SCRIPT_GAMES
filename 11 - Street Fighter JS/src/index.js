f6eb8d4179cc08f10aef4ec3ed3a23fb3583a61a
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-01 23:57:21 -0300

    time to next frame

--- 11 - Street Fighter JS/src/index.js
+++ 11 - Street Fighter JS/src/index.js
@@ -1,5 +1,5 @@
-import { Ryu } from './ryu.js';
-import { Ken } from './ken.js';
+import { Ryu } from './entities/Fighters/Ryu.js';
+import { Ken } from './entities/Fighters/Ken.js';
 import { Stage } from './stage.js';
 
 
@@ -15,13 +15,19 @@ window.onload = function(){
     canvasEl.width = GameViewport.WIDTH;
     canvasEl.height = GameViewport.HEIGHT;
 
-    const ken = new Ken(80, 110, -1);
-    const ryu = new Ryu(80,110,1);
+    const ken = new Ken(80, 110, -50);
+    const ryu = new Ryu(80,110,50);
     const stage = new Stage();
 
-    function frame(){
-        ryu.update(context);
-        ken.update(context);
+    let previousTime = 0;
+    let secondsPassed = 0;
+
+    function frame(time){
+        secondsPassed = (time - previousTime)/1000;
+        previousTime = time;
+
+        ryu.update(secondsPassed, context);
+        ken.update(secondsPassed,context);
         stage.draw(context);
         ryu.draw(context);
         ken.draw(context);
