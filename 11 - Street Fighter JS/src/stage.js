e86f701924d88b967fe0ba694fa301bef4c23707
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-02 00:11:24 -0300

    add update method on stage class

--- 11 - Street Fighter JS/src/stage.js
+++ 11 - Street Fighter JS/src/stage.js
@@ -1,14 +1,9 @@
-
-const  background = document.querySelector('img[alt="background"]');
-
-export function drawBackground(context){
-    context.drawImage(background, 0,0);
-}
-
 export class Stage{
     constructor(){
         this.image = document.querySelector('img[alt="background"]');
     }
+
+    update(){}
 
     draw(context){
         context.drawImage(this.image,0,0);
