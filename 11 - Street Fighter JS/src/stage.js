f8ec57c0af65f6d2d6b3b8075021991bd507c4c6
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-01 22:35:15 -0300

    create class stage

--- 11 - Street Fighter JS/src/stage.js
+++ 11 - Street Fighter JS/src/stage.js
@@ -1,6 +1,16 @@
+
 const  background = document.querySelector('img[alt="background"]');
 
 export function drawBackground(context){
     context.drawImage(background, 0,0);
 }
 
+export class Stage{
+    constructor(){
+        this.image = document.querySelector('img[alt="background"]');
+    }
+
+    draw(context){
+        context.drawImage(this.image,0,0);
+    }
+}
