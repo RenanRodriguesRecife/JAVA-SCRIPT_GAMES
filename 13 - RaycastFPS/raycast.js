18ff7c625872aa202c01b357236b4493bbdb714b
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-14 04:17:32 -0300

    eraseCanvas and level1

--- 13 - RaycastFPS/raycast.js
+++ 13 - RaycastFPS/raycast.js
@@ -5,6 +5,19 @@ var FPS = 60;
 var canvasWidth = 500;
 var canvasHeight = 500;
 
+var level1 = [
+    [1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,0,0,0,0,0,1],
+    [1,0,1,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,1,0,0,1],
+    [1,0,0,0,0,1,1,0,0,1],
+    [1,0,0,1,1,1,0,0,0,1],
+    [1,0,0,0,0,0,0,1,0,1],
+    [1,0,0,0,0,0,0,1,0,1],
+    [1,1,1,1,1,1,1,1,1,1],
+]
+
 function init(){
     canvas = document.getElementById('canvas');
     console.log(canvas)
@@ -12,4 +25,15 @@ function init(){
 
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
+
+        setInterval(function(){main();},1000/FPS)
+}
+
+function eraseCanvas(){
+    canvas.width = canvas.width;
+    canvas.height = canvas.height;
+}
+
+function main(){
+    console.log("frame")
 }
