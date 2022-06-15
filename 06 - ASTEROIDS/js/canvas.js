2e27a5221d7cfb5c7bfc05dcc7ce23842d3eb92f
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-15 11:32:24 -0300

    canvas animation function and frame functionality

--- 06 - ASTEROIDS/js/canvas.js
+++ 06 - ASTEROIDS/js/canvas.js
@@ -12,5 +12,26 @@ var Canvas = Class.extend({
         })(this.canvas.getContext("2d"));
 
         document.body.appendChild(this.canvas);
+    },
+
+    animate: function(loop){
+        var rf = (function() {
+            return window.requestAnimationFrame     ||
+                window.webkitRequestAnimationFrame  ||
+                window.mozRequestAnimationFrame     ||
+                window.oRequestAnimationFrame       ||
+                window.msRequestAnimationFrame      ||
+                //callback
+                function(cb, el){
+                    window.setTimeout(cb, 1000/60);
+                }
+        })();
+
+        var self = this;
+        var l = function(){
+            loop();
+            rf(l, self.canvas);
+        }
+        rf(l,this.canvas);
     }
 })
