a7f126295fc24cef135f0dc3b3b2f33f5692be59
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-15 11:36:32 -0300

    base state system

--- 06 - ASTEROIDS/js/main.js
+++ 06 - ASTEROIDS/js/main.js
@@ -1,9 +1,29 @@
+var States = {
+    NO_CHANGE: 0,
+    MENU: 1,
+    GAME: 2,
+    END: 3
+}
+
+
 var Game = Class.extend({
     init: function(){
         this.canvas = new Canvas(640, 480);
+
+        this.currentState = null;
+        this.nextState = States.GAME;
     },
 
     run: function(){
-
+        this.canvas.animate(function(){
+           if(self.nextState !== States.NO_CHANGE){
+            switch(self.nextState){
+                case States.MENU:
+                case States.GAME:
+                case States.END:
+            }
+            self.nextState = States.NO_CHANGE;
+           }
+        });
     }
 })
