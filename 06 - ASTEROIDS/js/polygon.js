b160cff47776d538eee56700089272a7876920f8
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-18 10:05:12 -0300

    create poly class

---
+++ 06 - ASTEROIDS/js/polygon.js
@@ -0,0 +1,16 @@
+var Polygon = Class.extend({
+
+    init: function(p){
+        this.points = p.slice(0);
+    },
+
+    rotate: function(theta){
+        
+    },
+    scale:function(c){
+
+    },
+    hasPoint:function(ox, oy, x, y){
+
+    }
+})
