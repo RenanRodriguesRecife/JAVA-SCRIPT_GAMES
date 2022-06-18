baa77fcf7622d2658fcab0b230a01e0f02ace691
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-18 10:07:56 -0300

    minor change

--- 06 - ASTEROIDS/js/state.js
+++ 06 - ASTEROIDS/js/state.js
@@ -2,7 +2,7 @@ var State = Class.extend({
     init: function(game){
         this.game = game;
     },
-    handleInput: function(){},
+    handleInputs: function(){},
     update: function(){},
     render: function(ctx){}
 })
