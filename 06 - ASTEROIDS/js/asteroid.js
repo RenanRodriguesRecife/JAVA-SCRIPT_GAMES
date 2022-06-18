ab57feb9b0855f78003cf4c4238fa18b0edd45e5
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-18 16:15:22 -0300

    asteroid move and rotate

--- 06 - ASTEROIDS/js/asteroid.js
+++ 06 - ASTEROIDS/js/asteroid.js
@@ -4,6 +4,21 @@ var Asteroid = Polygon.extend({
         this.x = x;
         this.y = y;
         this.scale(s);
+
+        this.rotAngle = 0.01*(Math.random()*2-1);
+
+        var r = 2*Math.PI*Math.random();
+        var v = Math.random()*4 + 1;
+        this.vel = {
+            x: v*Math.cos(r),
+            y: v*Math.sin(r)
+        }
+    },
+
+    update: function(){
+        this.x += this.vel.x;
+        this.y += this.vel.y;
+        this.rotate(this.rotAngle)
     },
     draw: function(ctx){
         ctx.drawPolygon(this, this.x, this.y);
