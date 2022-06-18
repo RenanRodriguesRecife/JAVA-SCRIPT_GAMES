11425503e2c2c426939fb81547fc147a0bf99998
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-18 16:03:32 -0300

    random asteroid draw

--- 06 - ASTEROIDS/js/gameState.js
+++ 06 - ASTEROIDS/js/gameState.js
@@ -1,17 +1,29 @@
+var Points = {
+    ASTEROIDS:[
+        [-4,-2,-2,-4,0,-2,2,-4,4,-2,3,0,4,2,1,4,-2,4,-4,2,-4,-2],
+        [-3,0,-4,-2,-2,-4,0,-3,2,-4,4,-2,2,-1,4,1,2,4,-1,3,-2,4,-4,2,-3,0],
+        [-2,0,-4,-1,-1,-4,2,-4,4,-1,4,1,2,4,0,4,0,1,-2,4,-4,1,-2,0],
+        [-1,-2,-2,-4,1,-4,4,-2,4,-1,1,0,4,2,2,4,1,3,-2,4,-4,1,-4,1,-4,-2,-1,-2],
+        [-4,-2,-2,-4,2,-4,4,-2,4,2,2,4,-2,4,-4,2,-4,-2]
+    ]
+}
+
+
 var GameState = State.extend({
     init: function(game){
         this._super(game);
 
-        this.poly = new Polygon([-1,-1,1,1,-1,1,-1,-1])
-        this.poly.scale(50);
+        var n = Math.round(Math.random()*(Points.ASTEROIDS.length-1))
+        this.poly = new Asteroid(Points.ASTEROIDS[n],10,100,100)
+        
     },  
 
     update: function(){
-        this.poly.rotate(0.01);
+        
     },
 
     render: function(ctx){
         ctx.clearAll();
-        ctx.drawPolygon(this.poly, 100, 100);
+        this.poly.draw(ctx);
     }
 })
