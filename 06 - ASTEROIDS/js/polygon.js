ce52c38f190ca177746b5590f527081c27acf5eb
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-18 10:56:15 -0300

    function rotate

--- 06 - ASTEROIDS/js/polygon.js
+++ 06 - ASTEROIDS/js/polygon.js
@@ -5,7 +5,16 @@ var Polygon = Class.extend({
     },
 
     rotate: function(theta){
-        
+        var c = Math.cos(theta);
+        var s = Math.sin(theta);
+
+        for(var i = 0, len=this.points.length;i<len;i+=2){
+            var x = this.points[i];
+            var y = this.points[i+1];
+
+            this.points[i] = c*x - s*y;
+            this.points[i + 1] = s*x + c*y;
+        }
     },
     scale:function(c){
         for(var i = 0, len = this.points.length; i<len; i++){
