f69376d5b91cc265e2058310f66c39099aca8990
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-20 12:31:04 -0300

    minor fix

--- 11 - Street Fighter JS/src/entities/Fighters/Ryu.js
+++ 11 - Street Fighter JS/src/entities/Fighters/Ryu.js
@@ -9,7 +9,7 @@ export class Ryu extends Fighter{
             ['forwards-2', [78, 131, 60, 89]],
             ['forwards-3', [152, 128, 64, 92]],
             ['forwards-4', [229, 130, 63, 90]],
-            ['forwards-5', [387, 128, 54, 91]],
+            ['forwards-5', [307, 128, 54, 91]],
             ['forwards-6', [371, 128, 50, 89]],]);
     }
     
