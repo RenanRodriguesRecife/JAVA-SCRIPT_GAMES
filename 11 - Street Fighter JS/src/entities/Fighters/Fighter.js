c0f62767dbbc8565c7d9c1476c21076ccc8c62a0
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-20 09:18:14 -0300

    add delay between animation frame

--- 11 - Street Fighter JS/src/entities/Fighters/Fighter.js
+++ 11 - Street Fighter JS/src/entities/Fighters/Fighter.js
@@ -5,18 +5,27 @@ export class Fighter{
         this.position = { x, y};
         this.velocity = velocity;
         this.frames = new Map();
+        this.animationFrame = 1;
+        this.animationTimer = 0;
     }
 
-    update(secondsPassed,context){
-        const [, ,width] = this.frame;
-        this.position.x += this.velocity * secondsPassed;
+    update(time,context){
+        const [, ,width] = this.frames.get(`forwards-${this.animationFrame}`);
+
+        if(time.previous > this.animationTimer * 60){
+            this.animationTimer = time.previous;
+            this.animationFrame++;
+            if(this.animationFrame > 6) this.animationFrame =1;    
+        }
+        
+        this.position.x += this.velocity * time.secondsPassed;
         if(this.position.x > context.canvas.width - width || this.position.x < 0){
             this.velocity = -this.velocity;
         }
     }
 
     draw(context){
-        const[x, y, width, height] = this.frame;
+        const[x, y, width, height] = this.frames.get(`forwards-${this.animationFrame}`);
         context.drawImage(this.image, x, y, width, height, this.position.x, this.position.y, width, height);
     }
 }
