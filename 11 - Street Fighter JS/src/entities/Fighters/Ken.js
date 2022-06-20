f1415932277605dedcc5e793ea210719c8b4c4b8
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-20 12:50:18 -0300

    add origin point reference

--- 11 - Street Fighter JS/src/entities/Fighters/Ken.js
+++ 11 - Street Fighter JS/src/entities/Fighters/Ken.js
@@ -4,12 +4,13 @@ export class Ken extends Fighter{
     constructor(x, y, velocity){
         super('Ken', x, y, velocity);
         this.image = document.querySelector('img[alt="ken"');
-        this.frames = new Map([ 
-            ['forwards-1', [8, 872, 53, 83]],
-            ['forwards-2', [78, 867, 60, 80]],
-            ['forwards-3', [140, 866, 64, 90]],
-            ['forwards-4', [215, 865, 63, 89]],
-            ['forwards-5', [288, 866, 54, 89]],
-            ['forwards-6', [357, 867, 50, 89]],]);
+        this.frames = new Map([
+            ['forwards-1', [[8, 872, 53, 83],[27,81]]],
+            ['forwards-2', [[78, 867, 60, 80],[35,86]]],
+            ['forwards-3', [[140, 866, 64, 90],[35,87]]],
+            ['forwards-4', [[215, 865, 63, 89],[29,88]]],
+            ['forwards-5', [[288, 866, 54, 89],[25,87]]],
+            ['forwards-6', [[357, 867, 50, 89],[25,86]]],
+        ]);
     }
 }
--- 11 - Street Fighter JS/src/entities/Fighters/Ryu.js
+++ 11 - Street Fighter JS/src/entities/Fighters/Ryu.js
@@ -5,12 +5,13 @@ export class Ryu extends Fighter{
         super('Ryu', x, y, velocity);
         this.image = document.querySelector('img[alt="ryu"');
         this.frames = new Map([ 
-            ['forwards-1', [9, 136, 53, 83]],
-            ['forwards-2', [78, 131, 60, 89]],
-            ['forwards-3', [152, 128, 64, 92]],
-            ['forwards-4', [229, 130, 63, 90]],
-            ['forwards-5', [307, 128, 54, 91]],
-            ['forwards-6', [371, 128, 50, 89]],]);
+            ['forwards-1', [[9, 136, 53, 83],[27,81]]],
+            ['forwards-2', [[78, 131, 60, 89],[35,86]]],
+            ['forwards-3', [[152, 128, 64, 92],[35,87]]],
+            ['forwards-4', [[229, 130, 63, 90],[29,88]]],
+            ['forwards-5', [[307, 128, 54, 91],[25,87]]],
+            ['forwards-6', [[371, 128, 50, 89],[25,86]]],
+        ]);
     }
     
 }
