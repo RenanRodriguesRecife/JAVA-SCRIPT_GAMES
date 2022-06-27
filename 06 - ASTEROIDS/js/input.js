4065ed2a4c3aafb3562fc7377d2fd4ff30fe2c44
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-27 15:29:12 -0300

    key up and down function handler

--- 06 - ASTEROIDS/js/input.js
+++ 06 - ASTEROIDS/js/input.js
@@ -6,7 +6,7 @@ var InputHandeler = Class.extend({
 
         for(key in keys){
             var code = keys[key];
-            
+
             this.keys[code] = key;
             this.down[key] = false;
             this.pressed[key] = false;
@@ -14,10 +14,29 @@ var InputHandeler = Class.extend({
 
         var self = this;
         document.addEventListener("keydown",function(evt){
-
+            if(self.keys[evt.keyCode]){
+                self.down[self.keys[evt.keyCode]] = true;
+            }
         });
         document.addEventListener("keyup",function(evt){
+            if(self.keys[evt.keyCode]){
+                self.down[self.keys[evt.keyCode]] = true;
+                self.pressed[self.keys[evt.keyCode]] = false;
 
+            }
         });
+    },
+
+    isDown: function(key){
+        return this.down[key];
+    },
+
+    isPressed: function(key){
+        if(this.pressed[key]){
+            return false;
+        }else if(this.down[key]){
+            return this.pressed[key] = true;
+        }
+        return false;
     }
 })
