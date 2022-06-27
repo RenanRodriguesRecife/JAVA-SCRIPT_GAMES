02ef66a75c464026fe902f3bde948d8db27dbdf8
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-27 15:17:16 -0300

    base input class

---
+++ 06 - ASTEROIDS/js/input.js
@@ -0,0 +1,15 @@
+var InputHandeler = Class.extend({
+    init: function(keys){
+        this.keys = {},
+        this.down = {},
+        this.pressed = {};
+
+        var self = this;
+        document.addEventListener("keydown",function(evt){
+
+        });
+        document.addEventListener("keyup",function(evt){
+
+        });
+    }
+})
