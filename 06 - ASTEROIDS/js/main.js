a4defdaeb11be296c7965765a128f68a42cafea0
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-29 22:32:55 -0300

    input handler keys

--- 06 - ASTEROIDS/js/main.js
+++ 06 - ASTEROIDS/js/main.js
@@ -10,6 +10,14 @@ var Game = Class.extend({
     init: function(){
         this.canvas = new Canvas(640, 480);
 
+        this.input = new InputHandeler({
+            left: 37,
+            up: 38,
+            right: 39,
+            down: 40,
+            spacebar: 32
+        });
+
         this.canvas.ctx.strokeStyle = "#fff";
         this.currentState = null;
         this.nextState = States.GAME;
@@ -32,7 +40,7 @@ var Game = Class.extend({
             }
             self.nextState = States.NO_CHANGE;
            }
-           self.currentState.handleInputs();
+           self.currentState.handleInputs(self.input);
            self.currentState.update();
            self.currentState.render(self.canvas.ctx);
         });
