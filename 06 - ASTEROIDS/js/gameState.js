ecd9fb16ea5a60ac029211f893d7586dc376b76a
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-06-30 22:09:19 -0300

    add velo function base

--- 06 - ASTEROIDS/js/gameState.js
+++ 06 - ASTEROIDS/js/gameState.js
@@ -39,12 +39,13 @@ var GameState = State.extend({
 
     handleInputs: function(input){
         if(input.isDown('right')){
-            this.ship.rotate(0.06)    }
+            this.ship.rotate(0.06)    
+        }
         if(input.isDown('left')){
             this.ship.rotate(-0.06)
         }
         if(input.isDown('up')){
-            this.ship.addVel()/
+            this.ship.addVel();
         }
     },
 
